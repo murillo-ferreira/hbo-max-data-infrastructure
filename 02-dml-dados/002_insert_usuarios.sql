6994510d1cdd72dd35967c50109befa90bd04523
Murillo Ferreira <murillo.ds.ferreira@gmail.com>
2026-05-21 14:29:37 -0300

    refactor: unifica carga de dados fakes nos scripts de tabelas

--- 02-dml-dados/002_insert_usuarios.sql
+++ 02-dml-dados/002_insert_usuarios.sql
@@ -1,10 +1,10 @@
 USE hbo_db;
 GO
 
--- 1. Criamos a variável para controlar o loop
+-- Variável para controlar o loop
 DECLARE @contador INT = 1;
 
--- 2. Iniciamos o laço de repetição para gerar 1000 usuários
+-- Inicia o laço de repetição para gerar 1000 usuários
 WHILE @contador <= 1000
 BEGIN
     
@@ -12,11 +12,12 @@ BEGIN
     VALUES (
         'Usuario ' + CAST(@contador AS VARCHAR(5)),               -- Nome: 'Usuario 1', 'Usuario 2'...
         'usuario' + CAST(@contador AS VARCHAR(5)) + '@email.com',  -- Email único (Garante a restrição UNIQUE)
-        'senha123',                                               -- Senha simples em texto puro
+        'senha123',
+        -- Cria um por dia                                       -- Senha simples em texto puro
         GETDATE() - @contador                                     -- Data: Cria um por dia retroativo no passado
     );
 
-    -- 3. Soma +1 no contador para avançar para o próximo usuário
+    -- Soma +1 no contador para avançar para o próximo usuário
     SET @contador = @contador + 1;
 END;
 GO
