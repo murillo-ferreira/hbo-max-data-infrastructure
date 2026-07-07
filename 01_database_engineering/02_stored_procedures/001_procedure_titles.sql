5df3e070c5bfac55f6af9150484e8a45cb4564e9
Murillo Ferreira <murillo.ds.ferreira@gmail.com>
2026-07-07 17:23:21 -0300

    refactor(database): update sp_upserttitles

--- 01_database_engineering/02_stored_procedures/001_procedure_titles.sql
+++ 01_database_engineering/02_stored_procedures/001_procedure_titles.sql
@@ -2,16 +2,17 @@ USE hbo_db;
 GO
 
 CREATE OR ALTER PROCEDURE dbo.sp_UpsertTitles
-    @id NVARCHAR(50),
-    @title VARCHAR(255),
+    @tmdb_id INT,
+    -- Mudamos de @id para @tmdb_id
     @imdb_id NVARCHAR(50),
+    @title VARCHAR(255),
     @type VARCHAR(50),
     @release_year SMALLINT,
-    @runtime TINYINT,
+    @runtime SMALLINT,
     @genres NVARCHAR(255),
     @budget INT,
     @origin_country NVARCHAR(50),
-    @seasons TINYINT,
+    @seasons SMALLINT,
     @vote_average FLOAT,
     @vote_count INT,
     @popularity FLOAT
@@ -21,7 +22,7 @@ BEGIN TRY
     BEGIN TRANSACTION
         MERGE dbo.titles WITH (HOLDLOCK) AS t
         USING (SELECT
-    @id AS id,
+    @tmdb_id AS tmdb_id, -- Mapeado aqui
     @imdb_id AS imdb_id,
     @title AS title,
     @type AS type,
@@ -35,15 +36,17 @@ BEGIN TRY
     @vote_count AS vote_count,
     @popularity AS popularity
               ) AS s
-        ON t.title = s.title AND t.release_year = s.release_year
+        ON t.tmdb_id = s.tmdb_id AND t.type = s.type
         WHEN MATCHED THEN
             UPDATE SET 
                 t.imdb_id = s.imdb_id,
                 t.vote_average = s.vote_average, 
                 t.popularity = s.popularity
+                
         WHEN NOT MATCHED THEN
-            INSERT (id, imdb_id, title, type, release_year, runtime, genres, budget, origin_country, seasons, vote_average, vote_count, popularity)
-            VALUES (s.id, s.imdb_id, s.title, s.type, s.release_year, s.runtime, s.genres, s.budget, s.origin_country, s.seasons, s.vote_average, s.vote_count, s.popularity);
+            INSERT (tmdb_id, imdb_id, title, type, release_year, runtime, genres, budget, origin_country, seasons, vote_average, vote_count, popularity)
+            VALUES (s.tmdb_id, s.imdb_id, s.title, s.type, s.release_year, s.runtime, s.genres, s.budget, s.origin_country, s.seasons, s.vote_average, s.vote_count, s.popularity);
+            
     COMMIT TRANSACTION
 END TRY
 BEGIN CATCH
