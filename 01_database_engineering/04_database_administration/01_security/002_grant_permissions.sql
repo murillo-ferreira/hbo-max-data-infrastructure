c0bee7f73ca86a78bd52da414944b88e8d334cf4
Murillo Ferreira <murillo.ds.ferreira@gmail.com>
2026-07-22 13:39:19 -0300

    chore(database): update python pipeline role permissions - grant execution permissions for new stored procedures to python_pipeline_role - update select and insert privileges on core subscription tables

--- 01_database_engineering/04_database_administration/01_security/002_grant_permissions.sql
+++ 01_database_engineering/04_database_administration/01_security/002_grant_permissions.sql
@@ -7,7 +7,15 @@ GRANT EXECUTE ON OBJECT::dbo.sp_UpsertUsers TO python_pipeline_role;
 GRANT EXECUTE ON OBJECT::dbo.sp_UpsertSubscriptions TO python_pipeline_role;
 GRANT EXECUTE ON OBJECT::dbo.sp_InsertWatchHistory TO python_pipeline_role;
 GO
-GRANT INSERT ON OBJECT::dbo.pipeline_logs TO python_pipeline_role;
+GRANT EXECUTE ON OBJECT::dbo.sp_InsertSubscription TO python_pipeline_role;
 GO
+GRANT EXECUTE ON OBJECT::dbo.sp_CloseSubscription TO python_pipeline_role;
+GRANT EXECUTE ON OBJECT::dbo.sp_ChurnRate TO python_pipeline_role;
 GRANT SELECT ON OBJECT::dbo.titles TO python_pipeline_role;
 GO
+GRANT SELECT ON OBJECT::dbo.users TO python_pipeline_role;
+GRANT SELECT ON OBJECT::dbo.subscriptions TO python_pipeline_role;
+GRANT INSERT ON OBJECT:: dbo.subscriptions TO python_pipeline_role;
+GRANT INSERT ON OBJECT::dbo.pipeline_logs TO python_pipeline_role;
+GO
+GRANT INSERT ON OBJECT::dbo.titles TO python_pipeline_role;
